dee7c8807d870b43f670f440d2df4de2de2d8151
Magnus Orskov <magnusorskov@hotmail.com>
2025-09-03 14:20:40 +0200

    finish

--- src/aboutModule.js
+++ src/aboutModule.js
@@ -1,6 +1,29 @@
 const createAboutPage = () => {
     document.querySelector("#about-button").classList.add("selected-button");
+    const content = document.querySelector("#content");
+    content.classList.add('about-active');
 
+    const h1 = document.createElement("h1");
+    h1.textContent = "About us";
+    content.appendChild(h1);
+
+    const aboutUsContainer = document.createElement("div");
+    aboutUsContainer.classList.add("about-us-container");
+
+    const p1 = document.createElement("p");
+    p1.textContent = "\"We Bake\" is more than just our name; it's the heart of everything we do. Our story begins in" +
+        " the quiet, early hours of the morning, long before the sun graces the sky over Viby. It starts with a deep-seated love for the timeless art of baking—a passion that you can taste in every loaf, pastry, and cake we create. For us, this is not a job; it's a heartfelt tradition of transforming simple, pure ingredients into moments of warmth and delight.";
+    aboutUsContainer.appendChild(p1);
+
+    const p2 = document.createElement("p");
+    p2.textContent = "Our philosophy is built on the belief that the finest flavors are born from the purest sources. We are committed to meticulously sourcing our ingredients, favouring local Danish producers whenever possible. From the organic, stone-ground flour milled here in Jylland to the fresh, seasonal berries from nearby farms, every component is chosen to be a true testament to quality. This dedication to purity is the foundation of the honest, lovingly prepared creations we offer our community each day."
+    aboutUsContainer.appendChild(p2);
+
+    const p3 = document.createElement("p");
+    p3.textContent = "Ultimately, our greatest joy comes from being your neighbourhood bakery. We cherish the role we play in your daily routines, special occasions, and simple moments of comfort. We believe our work is more than a craft; it's a way to connect with and bring happiness to the community we are so proud to be a part of."
+    aboutUsContainer.appendChild(p3);
+
+    content.appendChild(aboutUsContainer);
 }
 
 export {createAboutPage}
--- src/menuModule.js
+++ src/menuModule.js
@@ -1,8 +1,15 @@
 import bread1 from "./images/loaf-homemade-bread-wooden-cutting-board.jpg";
+import bread2 from "./images/loaf-bread-wooden-table.jpg";
+import bread3 from "./images/pexels-chance-agrella-2154641066-33735154.jpg";
+
 const createMenuPage = () => {
     document.querySelector("#menu-button").classList.add("selected-button");
     const content = document.querySelector("#content");
     content.classList.add('menu-active');
+
+    const menuTitle = document.createElement("h1");
+    menuTitle.textContent = "Menu"
+    content.appendChild(menuTitle);
 
     const menuContainer = document.createElement("div");
     menuContainer.classList.add("menu-container");
@@ -17,9 +24,9 @@ const createMenuPage = () => {
     const itemOneTextContainer = document.createElement("div");
     itemOneTextContainer.classList.add("item-text-container");
     const itemOneName = document.createElement("h3");
-    itemOneName.textContent = "itemOne title";
+    itemOneName.textContent = "The Hearth Loaf";
     const itemOneDescription = document.createElement("p");
-    itemOneDescription.textContent = "blabla bla bla bbadfsfsfd";
+    itemOneDescription.textContent = "Our signature country loaf, crafted over 36 hours from a wild yeast starter. It boasts a deep, tangy flavor, a wonderfully chewy crumb, and a crisp, caramelized crust. The perfect all-rounder for any occasion.";
 
     menuItemOneContainer.appendChild(imgItemOne);
     itemOneTextContainer.appendChild(itemOneName);
@@ -30,14 +37,17 @@ const createMenuPage = () => {
 
     const menuItemTwoContainer = document.createElement("div");
     menuItemTwoContainer.classList.add("menu-item-container");
-    const imgItemTwo = document.createElement("div");
+    const imgItemTwo = document.createElement("img");
+    imgItemTwo.src = bread2;
+    imgItemTwo.alt = "bread"
     imgItemTwo.id = "img-item-two";
+
     const itemTwoTextContainer = document.createElement("div");
     itemTwoTextContainer.classList.add("item-text-container");
     const ItemTwoName = document.createElement("h3");
-    ItemTwoName.textContent = "ItemTwo title";
+    ItemTwoName.textContent = "Harvest Grain Loaf";
     const ItemTwoDescription = document.createElement("p");
-    ItemTwoDescription.textContent = "blabla bla bla bbadfsfsfd";
+    ItemTwoDescription.textContent = "A dense and satisfying loaf, absolutely packed with a wholesome blend of sunflower, flax, and pumpkin seeds. Its rich, nutty flavor and moist crumb make it the perfect hearty choice for savory sandwiches or toasted with butter.";
 
     menuItemTwoContainer.appendChild(imgItemTwo);
     itemTwoTextContainer.appendChild(ItemTwoName);
@@ -48,14 +58,17 @@ const createMenuPage = () => {
 
     const menuItemThreeContainer = document.createElement("div");
     menuItemThreeContainer.classList.add("menu-item-container");
-    const imgItemThree = document.createElement("div");
+    const imgItemThree = document.createElement("img");
+    imgItemThree.src = bread3;
+    imgItemThree.alt = "bread"
     imgItemThree.id = "img-item-three";
+
     const itemThreeTextContainer = document.createElement("div");
     itemThreeTextContainer.classList.add("item-text-container");
     const ItemThreeName = document.createElement("h3");
-    ItemThreeName.textContent = "ItemThree title";
+    ItemThreeName.textContent = "Olive & Rosemary Sourdough";
     const ItemThreeDescription = document.createElement("p");
-    ItemThreeDescription.textContent = "blabla bla bla bbadfsfsfd";
+    ItemThreeDescription.textContent = "An artisan sourdough loaf studded with olives and rosemary.";
 
     menuItemThreeContainer.appendChild(imgItemThree);
     itemThreeTextContainer.appendChild(ItemThreeName);
